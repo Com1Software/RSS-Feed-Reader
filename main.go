414496b9011aca89a7f99223e54d3568327ae1f4
Lenovo 15 <information@com1software.com>
2025-03-14 15:35:41 -0400

    added

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"fmt"
+)
+
+// ----------------------------------------------------------------
+func main() {
+	fmt.Println("Video Web Server")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	xip := fmt.Sprintf("%s", GetOutboundIP())
+	port := "8080"
+	fmt.Println("Server running....")
+	fmt.Println("Listening on " + xip + ":" + port)
+
+	fmt.Println("")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		xdata := InitPage(xip)
+		fmt.Fprint(w, xdata)
+	})
+	Openbrowser(xip + ":" + port)
+	if err := http.ListenAndServe(xip+":"+port, nil); err != nil {
+		panic(err)
+	}
+}
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
+// Openbrowser : Opens default web browser to specified url
+func Openbrowser(url string) error {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start msedge"}
+
+	case "linux":
+		cmd = "chromium-browser"
+		args = []string{""}
+
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
+func InitPage(xip string) string {
+	//---------------------------------------------------------------------------
+	//----------------------------------------------------------------------------
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	//------------------------------------------------------------------------
+	xdata = xdata + "<title>RSS Feed Reader</title>"
+	//------------------------------------------------------------------------
+	xdata = xdata + "</head>"
+	//------------------------------------------------------------------------
+
+	xdata = xdata + "<body>"
+	xdata = xdata + "<center>"
+	xdata = xdata + "<H1>RSS Feed Reader</H1>"
+	//---------
+	xdata = xdata + "<body>"
+
+	xdata = xdata + "Host Port IP : " + xip
+	xdata = xdata + "<BR><BR>"
+
+	url := "https://forecast.weather.gov/MapClick.php?lat=41.5&lon=-81.7&unit=0&lg=english&FcstType=dwml"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching URL: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "HTTP error: %v\n", resp.Status)
+		os.Exit(1)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading response body: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(body))
+	xdata = xdata + string(body)
+	xdata = xdata + "<BR><BR>RSS Feed Reader"
+
+	//------------------------------------------------------------------------
+
+	xdata = xdata + "</center>"
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+}
